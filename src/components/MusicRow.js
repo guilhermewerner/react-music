dc3cb77f96ebf4a6e4b510ad7e9d3e9b4b27d175
GuilhermeWerner <guilhermeqwerner@gmail.com>
2020-08-30 15:02:12 -0300

    Test Music List

---
+++ src/components/MusicRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { List } from 'react-native-paper';
+
+export default function MusicRow() {
+    return (
+        <List.Item
+            title="Music Name"
+            description="Item description"
+            left={props => <List.Icon {...props} icon="music" />}
+        />
+    );
+}
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,13 +3,14 @@ import React from 'react';
 import AppBar from '../components/AppBar';
 
 import { Text } from 'react-native-paper';
+import MusicRow from '../components/MusicRow';
 
 export default function Index() {
     return (
         <React.Fragment>
             <AppBar />
             <React.Fragment>
-                <Text>Hello world!</Text>
+                <MusicRow />
             </React.Fragment>
         </React.Fragment>
     );
