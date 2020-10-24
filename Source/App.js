5040ea4c272fc4f9fbdfe970ff7d403769d4230d
GuilhermeWerner <guilhermeqwerner@gmail.com>
2020-10-24 09:14:39 -0300

    Setup Android Permissions

--- Source/App.js
+++ Source/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, PermissionsAndroid, Button } from "react-native";
 import { Provider as ThemeProvider } from "react-native-paper";
 
 import Index from "./Pages/Index";
 
 import Theme from "./Styles/Theme";
 
+async function RequestStoragePermission() {
+    try {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("READ_EXTERNAL_STORAGE GRANTED");
+        } else {
+            console.log("READ_EXTERNAL_STORAGE DENYED");
+        }
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
 export default function App() {
+    useEffect(() => {
+        RequestStoragePermission();
+    }), [];
+
     return (
         <ThemeProvider theme={Theme}>
             {/* 00b0ff */}
