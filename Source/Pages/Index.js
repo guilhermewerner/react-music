7df78142eb0ef73d5b2e5686fb23965344197412
GuilhermeWerner <guilhermeqwerner@gmail.com>
2020-10-24 09:14:54 -0300

    Read musics from internal storage

--- Source/Pages/Index.js
+++ Source/Pages/Index.js
@@ -6,6 +6,29 @@ import AppBar from "../Components/AppBar";
 
 import MusicRow from "../Components/MusicRow";
 
+import FileSystem from "react-native-fs";
+
+// file:///storage/emulated/0/Music
+// file:///storage/sdcard/Music
+// file:///storage/sdcard1/Music
+
+FileSystem.readDir("file:///storage/emulated/0/Music").then((result) => {
+    let musics = [];
+
+    result.forEach(file => {
+        if (file.isFile() && (file.name.endsWith(".mp3") || file.name.endsWith(".m4a"))) {
+            musics.push({
+                name: file.name,
+                path: file.path,
+            });
+        }
+    });
+
+    console.log(musics);
+}).catch((error) => {
+    console.log(error.message, error.code);
+});
+
 export default function Index() {
     return (
         <>
